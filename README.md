8e77480160bbc16fa2e5eaa278b970170bb8de93
milad siddiquey <miladsiddiquey@gmail.com>
2023-01-20 15:07:24 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React + Mui Dashboard
+
+
+## Acknowledgements
+ - [Build a COMPLETE React Admin Dashboard App](https://youtu.be/wYpCWwD1oz)
+
+## Demo
+
+https://reactmuidashboard.netlify.app
+
+
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { MyProSidebarProvider } from "./pages/global/sidebar/sidebarContext";
+
+import Topbar from "./pages/global/Topbar";
+
+import Dashboard from "./pages/dashboard";
+import Team from "./pages/team";
+import Invoices from "./pages/invoices";
+import Contacts from "./pages/contacts";
+import Form from "./pages/form";
+import Calendar from "./pages/calendar";
+import Bar from "./pages/bar";
+import Line from "./pages/line";
+import Pie from "./pages/pie";
+import FAQ from "./pages/faq";
+import Geography from "./pages/geography";
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyProSidebarProvider>
+          <div style={{ height: "100%", width: "100%" }}>
+            <main>
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/geography" element={<Geography />} />
+              </Routes>
+            </main>
+          </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
